56adafc8aad1681178c9e7bdeb19a33bd200c346
Aroma Plakolli <plakolli.aroma@outlook.com>
2025-10-28 10:54:56 +0100

    added modal and validation

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,25 @@
-const Modal = () => {
-    return (
-        <dialog open={true} className="relative z-10">
+import { useImperativeHandle, useState } from "react";
+import { useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({taskIndex, selectedProject, onDeleteProject, onClearTask, ref}) => {
+    const dialog = useRef();
+    const [modalType, setModalType] = useState('');
+
+    useImperativeHandle(ref, () => {
+        return {
+            open(type){
+                dialog.current.showModal();
+                setModalType(type);
+            },
+            closeModal(){
+                dialog.current.close();
+            }
+        }
+    })
+
+    return createPortal(
+        <dialog ref={dialog} className="relative z-10">
             <div className="fixed inset-0 z-10 w-screen overflow-y-auto bg-gray-700 opacity-75"></div>
             <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
                 <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
@@ -8,35 +27,34 @@ const Modal = () => {
                         <div className="sm:flex sm:items-start">
                             <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                                 <h3 className="text-2xl font-semibold text-stone-600">
-                                    Delete project
+                                    Delete {modalType === 'project' && 'project'}{modalType === 'task' && 'task'} 
                                 </h3>
                                 <div className="mt-5 mb-4">
                                     <div className="text-sm text-stone-500 whitespace-pre-wrap">
-                                        Are you sure you want to delete this project?
+                                        Are you sure you want to delete this {modalType === 'project' && 'project'}{modalType === 'task' && 'task'} ?
                                         This action cannot be undone.
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="z-15 bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                        <form method='dialog' className="z-15 bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                             <button
                             type="button"
+                            onClick={modalType === 'project' ? () => onDeleteProject(selectedProject) : () => onClearTask(selectedProject, taskIndex)}
                             className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 sm:ml-3 sm:w-auto"
                             >
                                 Delete
                             </button>
                             <button
-                            type="button"
-                            data-autofocus
                             className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs inset-ring inset-ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                             >
                                 Cancel
                             </button>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
-        </dialog>
+        </dialog>, document.getElementById('modal')
     )
 }
 
--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -1,35 +1,49 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import Modal from "./Modal";
 
 const ProjectDetails = ({project, error, selectedProject, onAddTasks, onDeleteProject, onClearTask}) => {
+    const dialog = useRef();
     const task = useRef();
+    const [taskIndex, setTaskIndex] = useState(null)
+
+    const handleModal = (type, index = null) => {
+        dialog.current.open(type);
+        if(type === 'task')
+            setTaskIndex(index);
+    }
 
     return (
-        <div className="w-8/12 mt-16">
-            <header className="pb-4 mb-4 border-b-2 border-stone-300">
-                <div className="flex items-center justify-between">
-                    <h1 className="text-3xl font-bold text-stone-600 mb-2">{project.title}</h1>
-                    <button onClick={() => onDeleteProject(selectedProject)} className="text-stone-600 hover:text-stone-950">Delete</button>
+        <>
+            <Modal ref={dialog} taskIndex={taskIndex} selectedProject={selectedProject} onDeleteProject={onDeleteProject} onClearTask={onClearTask} />
+            <div className="w-8/12 mt-16">
+                <header className="pb-4 mb-4 border-b-2 border-stone-300">
+                    <div className="flex items-center justify-between">
+                        <h1 className="text-3xl font-bold text-stone-600 mb-2">{project.title}</h1>
+                        {/* <button onClick={() => onDeleteProject(selectedProject)} className="text-stone-600 hover:text-stone-950">Delete</button> */}
+                        <button onClick={() => handleModal('project')} className="text-stone-600 hover:text-stone-950">Delete</button>
+                    </div>
+                    <p className="mb-4 text-stone-400">{project.dueDate}</p>
+                    <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
+                </header>
+                <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
+                <div className="flex items-center gap-4">
+                    <input ref={task} className={`w-64 px-2 py-1 rounded-sm bg-stone-200 ${error ? 'border border-red-500' : ''}`} />
+                    <button onClick={() => onAddTasks(task, selectedProject)} className="text-stone-700 hover:text-stone-950">Add Task</button>
                 </div>
-                <p className="mb-4 text-stone-400">{project.dueDate}</p>
-                <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
-            </header>
-            <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
-            <div className="flex items-center gap-4">
-                <input ref={task} className={`w-64 px-2 py-1 rounded-sm bg-stone-200 ${error ? 'border border-red-500' : ''}`} />
-                <button onClick={() => onAddTasks(task, selectedProject)} className="text-stone-700 hover:text-stone-950">Add Task</button>
+                {project.tasks.length > 0 ? 
+                (<ul  className="p-4 mt-8 rounded-md bg-stone-100">
+                {project.tasks.map((task, index) => (
+                    <li key={index} className="flex justify-between my-4">
+                        <p className="text-stone-800 my-4">{task}</p>
+                        {/* <button onClick={() => onClearTask(selectedProject, index)} className="text-stone-700 hover:text-red-500">Clear</button> */}
+                        <button onClick={() => handleModal('task', index)} className="text-stone-700 hover:text-red-500">Clear</button>
+                    </li>
+                ))}
+                </ul>)
+                :
+                (<p className="text-stone-800 my-4">This project does not have any tasks yet</p>)}
             </div>
-            {project.tasks.length > 0 ? 
-            (<ul  className="p-4 mt-8 rounded-md bg-stone-100">
-               {project.tasks.map((task, index) => (
-                <li key={index} className="flex justify-between my-4">
-                    <p className="text-stone-800 my-4">{task}</p>
-                    <button onClick={() => onClearTask(selectedProject, index)} className="text-stone-700 hover:text-red-500">Clear</button>
-                </li>
-               ))}
-            </ul>)
-            :
-            (<p className="text-stone-800 my-4">This project does not have any tasks yet</p>)}
-        </div>
+        </>
     )
 }
 
